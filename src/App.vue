0e8dc138217e170b0e886257f9412e0e293452bf
zsd6th <junghancho@gmail.com>
2021-04-18 16:11:31 +0900

    create scaffold

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+<script>
+/* eslint-disable */
+export default {
+  name: "App",
+};
+/* eslint-enable  */
+</script>
+
+<style>
+</style>
---
+++ src/views/LoginPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>TaskAgile</h1>
+  </div>
+</template>
+
+<script>
+/* eslint-disable */
+export default {
+  name: "LoginPage",
+};
+/* eslint-enable */
+</script>
